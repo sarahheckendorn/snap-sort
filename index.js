2ded4af15fc471ec6064d3d52ede9d4f81f71743
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-22 08:58:28 -0600

    first attempt at the JS challenge... still needs work

---
+++ index.js
@@ -0,0 +1,20 @@
+	const longString = '"It indicates a synchronic distortion in the areas emanating triolic waves. ' +
+		'The cerebellum, the cerebral cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. ' +
+		'Any device like that would produce high levels of triolic waves. ' +
+		'These walls have undergone some kind of selective molecular polarization. ' +
+		'I haven\'t determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators. ' +
+		'Sensors indicate human life forms 30 meters below the planet\'s surface. Stellar flares are increasing in magnitude and frequency. ' +
+		'Set course for Rhomboid Dronegar 006, warp seven. There\'s no evidence of an advanced communication network. ' +
+		'Total guidance system failure, with less than 24 hours\' reserve power. Shield effectiveness has been reduced 12 percent. ' +
+		'We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time. Resistance is futile."';
+
+	let words = longString().split(' ');
+
+	longString.sort(function() {
+
+	});
+
+	words.filter(words => words.match('sensors'));
+
+console.log(longString);
+
